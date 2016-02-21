a8750ce5b353eb3d63c75affe82211af7fbf8791
John <potatoking.john@gmail.com>
2016-02-21 12:11:17 -0700

    update plot data

--- plot3dgait.m
+++ plot3dgait.m
@@ -1,19 +1,19 @@
 for ii = 1:height(noS)
     
     
-%     plot3(noS.CLAVX(ii),noS.CLAVY(ii),noS.CLAVZ(ii),'Color','r',...
-%         'Marker','o','FaceColor','r','MarkerSize',9)
-cla
-scatter3(noS.RElbow_ARAnglesX(ii),noS.RElbow_ARAnglesY(ii),noS.RElbow_ARAnglesZ(ii),50,'r','filled')
-hold on
-if ii > 1
+    %     plot3(noS.CLAVX(ii),noS.CLAVY(ii),noS.CLAVZ(ii),'Color','r',...
+    %         'Marker','o','FaceColor','r','MarkerSize',9)
+    cla
+    scatter3(noS.RElbow_ARAnglesX(ii),noS.RElbow_ARAnglesY(ii),noS.RElbow_ARAnglesZ(ii),50,'r','filled')
+    hold on
+    if ii > 1
+        
+        
+        
+        plot3(noS.RElbow_ARAnglesX(1:ii-1),noS.RElbow_ARAnglesY(1:ii-1),noS.RElbow_ARAnglesZ(1:ii-1),'ko')
+    end
     
-    
-    
-    plot3(noS.RElbow_ARAnglesX(1:ii-1),noS.RElbow_ARAnglesY(1:ii-1),noS.RElbow_ARAnglesZ(1:ii-1),'ko')
-end
-
-pause(0.05)
+    pause(0.05)
     
 end
 
@@ -25,19 +25,19 @@ end
 for ii = 1:height(noS)
     
     
-%     plot3(noS.CLAVX(ii),noS.CLAVY(ii),noS.CLAVZ(ii),'Color','r',...
-%         'Marker','o','FaceColor','r','MarkerSize',9)
-cla
-scatter3(noS.CLAVX(ii),noS.CLAVY(ii),noS.CLAVZ(ii),50,'r','filled')
-hold on
-if ii > 1
+    %     plot3(noS.CLAVX(ii),noS.CLAVY(ii),noS.CLAVZ(ii),'Color','r',...
+    %         'Marker','o','FaceColor','r','MarkerSize',9)
+    cla
+    scatter3(noS.CLAVX(ii),noS.CLAVY(ii),noS.CLAVZ(ii),50,'r','filled')
+    hold on
+    if ii > 1
+        
+        
+        
+        plot3(noS.CLAVX(1:ii-1),noS.CLAVY(1:ii-1),noS.CLAVZ(1:ii-1),'ko')
+    end
     
-    
-    
-    plot3(noS.CLAVX(1:ii-1),noS.CLAVY(1:ii-1),noS.CLAVZ(1:ii-1),'ko')
-end
-
-pause(0.05)
+    pause(0.05)
     
 end
 
@@ -62,40 +62,40 @@ vz(1, t+dt) = (z(1, t+dt)-z(1, t))/dt
 for ii = 1:height(noS)
     
     
-%     plot3(noS.CLAVX(ii),noS.CLAVY(ii),noS.CLAVZ(ii),'Color','r',...
-%         'Marker','o','FaceColor','r','MarkerSize',9)
-cla
-scatter3(noS.LElbow_ARAnglesX(ii), noS.LElbow_ARAnglesY(ii), noS.LElbow_ARAnglesZ(ii),50,'r','filled')
-hold on
-scatter3(noS.LShoulder_ARAnglesX(ii), noS.LShoulder_ARAnglesY(ii), noS.LShoulder_ARAnglesZ(ii),50,'r','filled')
-
-xS = [noS.LElbow_ARAnglesX(ii) , noS.LShoulder_ARAnglesX(ii)];
-yS = [noS.LElbow_ARAnglesY(ii) , noS.LShoulder_ARAnglesY(ii)];
-zS = [noS.LElbow_ARAnglesZ(ii) , noS.LShoulder_ARAnglesZ(ii)];
-
-
-plot3(xS, yS, zS,'r')
-
-if ii > 1
+    %     plot3(noS.CLAVX(ii),noS.CLAVY(ii),noS.CLAVZ(ii),'Color','r',...
+    %         'Marker','o','FaceColor','r','MarkerSize',9)
+    cla
+    scatter3(noS.LElbow_ARAnglesX(ii), noS.LElbow_ARAnglesY(ii), noS.LElbow_ARAnglesZ(ii),50,'r','filled')
+    hold on
+    scatter3(noS.LShoulder_ARAnglesX(ii), noS.LShoulder_ARAnglesY(ii), noS.LShoulder_ARAnglesZ(ii),50,'r','filled')
     
-    scatter3(noS.LElbow_ARAnglesX(1:ii-1), noS.LElbow_ARAnglesY(1:ii-1), noS.LElbow_ARAnglesZ(1:ii-1),40,'k')
-    scatter3(noS.LShoulder_ARAnglesX(1:ii-1), noS.LShoulder_ARAnglesY(1:ii-1), noS.LShoulder_ARAnglesZ(1:ii-1),40,'k')
+    RxS = [noS.LElbow_ARAnglesX(ii) , noS.LShoulder_ARAnglesX(ii)];
+    RyS = [noS.LElbow_ARAnglesY(ii) , noS.LShoulder_ARAnglesY(ii)];
+    RzS = [noS.LElbow_ARAnglesZ(ii) , noS.LShoulder_ARAnglesZ(ii)];
     
     
-% xS2 = [noS.LElbow_ARAnglesX(1:ii-1)' ; noS.LShoulder_ARAnglesX(1:ii-1)'];
-% yS2 = [noS.LElbow_ARAnglesY(1:ii-1) ; noS.LShoulder_ARAnglesY(1:ii-1)];
-% zS2 = [noS.LElbow_ARAnglesZ(1:ii-1) ; noS.LShoulder_ARAnglesZ(1:ii-1)];
-
-line([noS.LElbow_ARAnglesX(1:ii-1)  noS.LShoulder_ARAnglesX(1:ii-1)]',...
-[noS.LElbow_ARAnglesY(1:ii-1)  noS.LShoulder_ARAnglesY(1:ii-1)]',...
-[noS.LElbow_ARAnglesZ(1:ii-1)  noS.LShoulder_ARAnglesZ(1:ii-1)]','Color','k');
-
-% plot3(xS2, yS2, zS2,'k')
+    plot3(RxS, RyS, RzS,'r')
     
+    if ii > 1
+        
+        scatter3(noS.LElbow_ARAnglesX(1:ii-1), noS.LElbow_ARAnglesY(1:ii-1), noS.LElbow_ARAnglesZ(1:ii-1),40,'k')
+        scatter3(noS.LShoulder_ARAnglesX(1:ii-1), noS.LShoulder_ARAnglesY(1:ii-1), noS.LShoulder_ARAnglesZ(1:ii-1),40,'k')
+        
+        
+        % xS2 = [noS.LElbow_ARAnglesX(1:ii-1)' ; noS.LShoulder_ARAnglesX(1:ii-1)'];
+        % yS2 = [noS.LElbow_ARAnglesY(1:ii-1) ; noS.LShoulder_ARAnglesY(1:ii-1)];
+        % zS2 = [noS.LElbow_ARAnglesZ(1:ii-1) ; noS.LShoulder_ARAnglesZ(1:ii-1)];
+        
+        line([noS.LElbow_ARAnglesX(1:ii-1)  noS.LShoulder_ARAnglesX(1:ii-1)]',...
+            [noS.LElbow_ARAnglesY(1:ii-1)  noS.LShoulder_ARAnglesY(1:ii-1)]',...
+            [noS.LElbow_ARAnglesZ(1:ii-1)  noS.LShoulder_ARAnglesZ(1:ii-1)]','Color','k');
+        
+        % plot3(xS2, yS2, zS2,'k')
+        
+        
+    end
     
-end
-
-pause(0.5)
+    pause(0.5)
     
 end
 
@@ -114,17 +114,17 @@ for ii = 1:height(noS)
     scatter3(noS.RElbow_ARAngles_X(ii), noS.RElbow_ARAngles_Y(ii), noS.RElbow_ARAngles_Z(ii),50,'r','filled')
     scatter3(noS.RShoulder_ARAngles_X(ii), noS.RShoulder_ARAngles_Y(ii), noS.RShoulder_ARAngles_Z(ii),50,'b','filled')
     
-    xS = [noS.RWrist_ARAngles_X(ii) , noS.RElbow_ARAngles_X(ii)];
-    yS = [noS.RWrist_ARAngles_Y(ii) , noS.RElbow_ARAngles_Y(ii)];
-    zS = [noS.RWrist_ARAngles_Z(ii) , noS.RElbow_ARAngles_Z(ii)];
+    RxS = [noS.RWrist_ARAngles_X(ii) , noS.RElbow_ARAngles_X(ii)];
+    RyS = [noS.RWrist_ARAngles_Y(ii) , noS.RElbow_ARAngles_Y(ii)];
+    RzS = [noS.RWrist_ARAngles_Z(ii) , noS.RElbow_ARAngles_Z(ii)];
     
-    xS2 = [noS.RElbow_ARAngles_X(ii) , noS.RShoulder_ARAngles_X(ii)];
-    yS2 = [noS.RElbow_ARAngles_Y(ii) , noS.RShoulder_ARAngles_Y(ii)];
-    zS2 = [noS.RElbow_ARAngles_Z(ii) , noS.RShoulder_ARAngles_Z(ii)];
+    RxS2 = [noS.RElbow_ARAngles_X(ii) , noS.RShoulder_ARAngles_X(ii)];
+    RyS2 = [noS.RElbow_ARAngles_Y(ii) , noS.RShoulder_ARAngles_Y(ii)];
+    RzS2 = [noS.RElbow_ARAngles_Z(ii) , noS.RShoulder_ARAngles_Z(ii)];
     
     % Connector
-    plot3(xS, yS, zS,'r')
-    plot3(xS2, yS2, zS2,'b')
+    plot3(RxS, RyS, RzS,'r')
+    plot3(RxS2, RyS2, RzS2,'b')
     
     if ii > 1
         
@@ -132,7 +132,7 @@ for ii = 1:height(noS)
         scatter3(noS.RElbow_ARAngles_X(1:ii-1), noS.RElbow_ARAngles_Y(1:ii-1), noS.RElbow_ARAngles_Z(1:ii-1),40,'r')
         
         scatter3(noS.RShoulder_ARAngles_X(1:ii-1), noS.RShoulder_ARAngles_Y(1:ii-1), noS.RShoulder_ARAngles_Z(1:ii-1),40,'b')
-
+        
         
         
         % xS2 = [noS.LElbow_ARAnglesX(1:ii-1)' ; noS.LShoulder_ARAnglesX(1:ii-1)'];
@@ -140,12 +140,12 @@ for ii = 1:height(noS)
         % zS2 = [noS.LElbow_ARAnglesZ(1:ii-1) ; noS.LShoulder_ARAnglesZ(1:ii-1)];
         
         line([noS.RWrist_ARAngles_X(1:ii-1)  noS.RElbow_ARAngles_X(1:ii-1)]',...
-             [noS.RWrist_ARAngles_Y(1:ii-1)  noS.RElbow_ARAngles_Y(1:ii-1)]',...
-             [noS.RWrist_ARAngles_Z(1:ii-1)  noS.RElbow_ARAngles_Z(1:ii-1)]','Color','r');
-         
+            [noS.RWrist_ARAngles_Y(1:ii-1)  noS.RElbow_ARAngles_Y(1:ii-1)]',...
+            [noS.RWrist_ARAngles_Z(1:ii-1)  noS.RElbow_ARAngles_Z(1:ii-1)]','Color','r');
+        
         line([noS.RElbow_ARAngles_X(1:ii-1)  noS.RShoulder_ARAngles_X(1:ii-1)]',...
-             [noS.RElbow_ARAngles_Y(1:ii-1)  noS.RShoulder_ARAngles_Y(1:ii-1)]',...
-             [noS.RElbow_ARAngles_Z(1:ii-1)  noS.RShoulder_ARAngles_Z(1:ii-1)]','Color','b');
+            [noS.RElbow_ARAngles_Y(1:ii-1)  noS.RShoulder_ARAngles_Y(1:ii-1)]',...
+            [noS.RElbow_ARAngles_Z(1:ii-1)  noS.RShoulder_ARAngles_Z(1:ii-1)]','Color','b');
         
         % plot3(xS2, yS2, zS2,'k')
         
@@ -153,5 +153,178 @@ for ii = 1:height(noS)
     end
     
     pause(0.2)
+    
+end
+
+%%
+
+noS = import_DBS_gait;
+
+%%
+
+xmin = min([noS.RWrist_ARAngles_X ; noS.RElbow_ARAngles_X ; noS.RShoulder_ARAngles_X ; ...
+    noS.LWrist_ARAngles_X ; noS.LElbow_ARAngles_X ; noS.LShoulder_ARAngles_X]);
+xmax = max([noS.RWrist_ARAngles_X ; noS.RElbow_ARAngles_X ; noS.RShoulder_ARAngles_X ; ...
+    noS.LWrist_ARAngles_X ; noS.LElbow_ARAngles_X ; noS.LShoulder_ARAngles_X]);
+
+ymin = min([noS.RWrist_ARAngles_Y ; noS.RElbow_ARAngles_Y ; noS.RShoulder_ARAngles_Y ; ...
+    noS.LWrist_ARAngles_Y ; noS.LElbow_ARAngles_Y ; noS.LShoulder_ARAngles_Y]);
+ymax = max([noS.RWrist_ARAngles_Y ; noS.RElbow_ARAngles_Y ; noS.RShoulder_ARAngles_Y ; ...
+    noS.LWrist_ARAngles_Y ; noS.LElbow_ARAngles_Y ; noS.LShoulder_ARAngles_Y]);
+
+zmin = min([noS.RWrist_ARAngles_Z ; noS.RElbow_ARAngles_Z ; noS.RShoulder_ARAngles_Z ; ...
+    noS.LWrist_ARAngles_Z ; noS.LElbow_ARAngles_Z ; noS.LShoulder_ARAngles_Z]);
+zmax = max([noS.RWrist_ARAngles_Z ; noS.RElbow_ARAngles_Z ; noS.RShoulder_ARAngles_Z ; ...
+    noS.LWrist_ARAngles_Z ; noS.LElbow_ARAngles_Z ; noS.LShoulder_ARAngles_Z]);
+
+useNorm = 0;
+
+for ii = 1:height(noS)
+    
+    cla
+    
+    % RIGHT POINTS
+    scatter3(noS.RWrist_ARAngles_X(ii), noS.RWrist_ARAngles_Y(ii), noS.RWrist_ARAngles_Z(ii),50,'r','filled')
+    hold on
+    scatter3(noS.RElbow_ARAngles_X(ii), noS.RElbow_ARAngles_Y(ii), noS.RElbow_ARAngles_Z(ii),50,'k','filled')
+    scatter3(noS.RShoulder_ARAngles_X(ii), noS.RShoulder_ARAngles_Y(ii), noS.RShoulder_ARAngles_Z(ii),50,'b','filled')
+    
+    text(noS.RWrist_ARAngles_X(ii),...
+        noS.RWrist_ARAngles_Y(ii),...
+        noS.RWrist_ARAngles_Z(ii),...
+        'RWrist',...
+        'Color','r',...
+        'VerticalAlignment','bottom',...
+        'HorizontalAlignment','center',...
+        'BackgroundColor',[0.8 0.8 0.8],...
+        'FontWeight','bold')
+    
+    text(noS.RElbow_ARAngles_X(ii),...
+        noS.RElbow_ARAngles_Y(ii),...
+        noS.RElbow_ARAngles_Z(ii),...
+        'RElbow',...
+        'Color','k',...
+        'VerticalAlignment','bottom',...
+        'HorizontalAlignment','center',...
+        'BackgroundColor',[0.8 0.8 0.8],...
+        'FontWeight','bold')
+    
+    text(noS.RShoulder_ARAngles_X(ii),...
+        noS.RShoulder_ARAngles_Y(ii),...
+        noS.RShoulder_ARAngles_Z(ii),...
+        'RShoulder',...
+        'Color','b',...
+        'VerticalAlignment','bottom',...
+        'HorizontalAlignment','center',...
+        'BackgroundColor',[0.8 0.8 0.8],...
+        'FontWeight','bold')
+    
+    RxS = [noS.RWrist_ARAngles_X(ii) , noS.RElbow_ARAngles_X(ii)];
+    RyS = [noS.RWrist_ARAngles_Y(ii) , noS.RElbow_ARAngles_Y(ii)];
+    RzS = [noS.RWrist_ARAngles_Z(ii) , noS.RElbow_ARAngles_Z(ii)];
+    
+    RxS2 = [noS.RElbow_ARAngles_X(ii) , noS.RShoulder_ARAngles_X(ii)];
+    RyS2 = [noS.RElbow_ARAngles_Y(ii) , noS.RShoulder_ARAngles_Y(ii)];
+    RzS2 = [noS.RElbow_ARAngles_Z(ii) , noS.RShoulder_ARAngles_Z(ii)];
+    
+    % Connector
+    plot3(RxS, RyS, RzS,'r')
+    plot3(RxS2, RyS2, RzS2,'b')
+    
+    
+    % LEFT POINTS
+    scatter3(noS.LWrist_ARAngles_X(ii), noS.LWrist_ARAngles_Y(ii), noS.LWrist_ARAngles_Z(ii),50,'r','filled')
+    scatter3(noS.LElbow_ARAngles_X(ii), noS.LElbow_ARAngles_Y(ii), noS.LElbow_ARAngles_Z(ii),50,'k','filled')
+    scatter3(noS.LShoulder_ARAngles_X(ii), noS.LShoulder_ARAngles_Y(ii), noS.LShoulder_ARAngles_Z(ii),50,'b','filled')
+    
+    text(noS.LWrist_ARAngles_X(ii),...
+        noS.LWrist_ARAngles_Y(ii),...
+        noS.LWrist_ARAngles_Z(ii),...
+        'LWrist',...
+        'Color','r',...
+        'VerticalAlignment','bottom',...
+        'HorizontalAlignment','center',...
+        'BackgroundColor',[0.8 0.8 0.8],...
+        'FontWeight','bold')
+    
+    text(noS.LElbow_ARAngles_X(ii),...
+        noS.LElbow_ARAngles_Y(ii),...
+        noS.LElbow_ARAngles_Z(ii),...
+        'LElbow',...
+        'Color','k',...
+        'VerticalAlignment','bottom',...
+        'HorizontalAlignment','center',...
+        'BackgroundColor',[0.8 0.8 0.8],...
+        'FontWeight','bold')
+    
+    text(noS.LShoulder_ARAngles_X(ii),...
+        noS.LShoulder_ARAngles_Y(ii),...
+        noS.LShoulder_ARAngles_Z(ii),...
+        'LShoulder',...
+        'Color','b',...
+        'VerticalAlignment','bottom',...
+        'HorizontalAlignment','center',...
+        'BackgroundColor',[0.8 0.8 0.8],...
+        'FontWeight','bold')
+    
+    LxS = [noS.LWrist_ARAngles_X(ii) , noS.LElbow_ARAngles_X(ii)];
+    LyS = [noS.LWrist_ARAngles_Y(ii) , noS.LElbow_ARAngles_Y(ii)];
+    LzS = [noS.LWrist_ARAngles_Z(ii) , noS.LElbow_ARAngles_Z(ii)];
+    
+    LxS2 = [noS.LElbow_ARAngles_X(ii) , noS.LShoulder_ARAngles_X(ii)];
+    LyS2 = [noS.LElbow_ARAngles_Y(ii) , noS.LShoulder_ARAngles_Y(ii)];
+    LzS2 = [noS.LElbow_ARAngles_Z(ii) , noS.LShoulder_ARAngles_Z(ii)];
+    
+    % Connector
+    plot3(LxS, LyS, LzS,'r')
+    plot3(LxS2, LyS2, LzS2,'b')
+    
+    
+    
+    
+    
+    if useNorm
+        
+        xlim([xmin xmax]);
+        ylim([ymin ymax]);
+        zlim([zmin zmax]);
+        
+    end
+    
+    xlabel('X Axis')
+    ylabel('Y Axis')
+    zlabel('Z Axis')
+    
+    pause(0.05)
+    
+end
+
+
+
+%%
+
+if ii > 1
+    
+    scatter3(noS.RWrist_ARAngles_X(1:ii-1), noS.RWrist_ARAngles_Y(1:ii-1), noS.RWrist_ARAngles_Z(1:ii-1),40,'r')
+    scatter3(noS.RElbow_ARAngles_X(1:ii-1), noS.RElbow_ARAngles_Y(1:ii-1), noS.RElbow_ARAngles_Z(1:ii-1),40,'r')
+    
+    scatter3(noS.RShoulder_ARAngles_X(1:ii-1), noS.RShoulder_ARAngles_Y(1:ii-1), noS.RShoulder_ARAngles_Z(1:ii-1),40,'b')
+    
+    
+    
+    % xS2 = [noS.LElbow_ARAnglesX(1:ii-1)' ; noS.LShoulder_ARAnglesX(1:ii-1)'];
+    % yS2 = [noS.LElbow_ARAnglesY(1:ii-1) ; noS.LShoulder_ARAnglesY(1:ii-1)];
+    % zS2 = [noS.LElbow_ARAnglesZ(1:ii-1) ; noS.LShoulder_ARAnglesZ(1:ii-1)];
+    
+    line([noS.RWrist_ARAngles_X(1:ii-1)  noS.RElbow_ARAngles_X(1:ii-1)]',...
+        [noS.RWrist_ARAngles_Y(1:ii-1)  noS.RElbow_ARAngles_Y(1:ii-1)]',...
+        [noS.RWrist_ARAngles_Z(1:ii-1)  noS.RElbow_ARAngles_Z(1:ii-1)]','Color','r');
+    
+    line([noS.RElbow_ARAngles_X(1:ii-1)  noS.RShoulder_ARAngles_X(1:ii-1)]',...
+        [noS.RElbow_ARAngles_Y(1:ii-1)  noS.RShoulder_ARAngles_Y(1:ii-1)]',...
+        [noS.RElbow_ARAngles_Z(1:ii-1)  noS.RShoulder_ARAngles_Z(1:ii-1)]','Color','b');
+    
+    % plot3(xS2, yS2, zS2,'k')
+    
     
 end
